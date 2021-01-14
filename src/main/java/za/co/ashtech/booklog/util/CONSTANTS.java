4dafcc9d0c5dc431ed3ea45b1cc2f51c70f9acac
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-01-15 01:36:22 +0200

    Add constants

---
+++ src/main/java/za/co/ashtech/booklog/util/CONSTANTS.java
@@ -0,0 +1,8 @@
+package za.co.ashtech.booklog.util;
+
+public class CONSTANTS {
+	
+	public final static String ERC001 = "ERC001";
+	public final static String ERC001_DESC = "Error creating book";
+
+}
