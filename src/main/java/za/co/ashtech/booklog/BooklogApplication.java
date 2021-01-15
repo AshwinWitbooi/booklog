01971b51670765077a981827620188e55498cd88
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-01-15 23:30:31 +0200

    Add Jasypt encryptionto app properties

--- src/main/java/za/co/ashtech/booklog/BooklogApplication.java
+++ src/main/java/za/co/ashtech/booklog/BooklogApplication.java
@@ -3,7 +3,10 @@ package za.co.ashtech.booklog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class BooklogApplication {
 
 	public static void main(String[] args) {
