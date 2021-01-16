e34dfe55d37664e373f7c24fe1b7706ef4ef7889
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-01-16 22:22:27 +0200

    disable HibernateJpaAutoConfiguration

--- src/main/java/za/co/ashtech/booklog/BooklogApplication.java
+++ src/main/java/za/co/ashtech/booklog/BooklogApplication.java
@@ -2,11 +2,12 @@ package za.co.ashtech.booklog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @EnableEncryptableProperties
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class BooklogApplication {
