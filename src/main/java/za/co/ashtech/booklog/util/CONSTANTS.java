a455811157933a6fbe895d59a16346203822b38a
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-01-16 22:25:38 +0200

    Add exception constants

--- src/main/java/za/co/ashtech/booklog/util/CONSTANTS.java
+++ src/main/java/za/co/ashtech/booklog/util/CONSTANTS.java
@@ -4,5 +4,9 @@ public class CONSTANTS {
 	
 	public final static String ERC001 = "ERC001";
 	public final static String ERC001_DESC = "Error creating book";
+	public final static String ERC002 = "ERC002";
+	public static final String ERC003 = "ERC003";
+	public static final String ERC004 = "ERC004";
+	public static final String ERC004_DESC = "ISBN number already exist";
 
 }
