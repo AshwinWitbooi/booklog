705409cee7aa783039b2901b66a969e26c2554cc
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-02-01 22:40:48 +0200

    Add error details and isbn validation

--- src/main/java/za/co/ashtech/booklog/util/CONSTANTS.java
+++ src/main/java/za/co/ashtech/booklog/util/CONSTANTS.java
@@ -2,11 +2,22 @@ package za.co.ashtech.booklog.util;
 
 public class CONSTANTS {
 	
+	/* Error code and descriptions */
 	public final static String ERC001 = "ERC001";
-	public final static String ERC001_DESC = "Error creating book";
+	public final static String ERC001_DESC = "Error creating book.";
 	public final static String ERC002 = "ERC002";
 	public static final String ERC003 = "ERC003";
 	public static final String ERC004 = "ERC004";
-	public static final String ERC004_DESC = "ISBN number already exist";
+	public static final String ERC004_DESC = "ISBN number already exist.";
+	public final static String ERC005 = "ERC005";
+	public static final String ERC006 = "ERC006";
+	public static final String ERC006_DESC = "Invalid action.";
+	public static final String ERC007 = null;
+	
+	/* Field value format pattern */
+	public static final String ISBN_PATTERN = "^\\d{3}\\-\\d{2}\\-\\d{6}\\-\\d{1}$";
+
+	
+
 
 }
