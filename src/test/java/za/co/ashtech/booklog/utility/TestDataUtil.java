6f8b6f4d7c9391359f0e9c5e38cd8ede96b509fb
Ashwin Witbooi <ashwin.witbooi@gmail.com>
2021-03-06 13:31:26 +0200

    Add username and JSOn to string method

--- src/test/java/za/co/ashtech/booklog/utility/TestDataUtil.java
+++ src/test/java/za/co/ashtech/booklog/utility/TestDataUtil.java
@@ -2,6 +2,9 @@ package za.co.ashtech.booklog.utility;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TestDataUtil {
 	
 	public static String getIsbn() {
@@ -10,6 +13,16 @@ public class TestDataUtil {
 				RandomStringUtils.randomNumeric(2)+"-"+	
 				RandomStringUtils.randomNumeric(6)+"-"+
 				RandomStringUtils.randomNumeric(1);
+	}
+	
+	public static String getUsername() {
+		return RandomStringUtils.randomAlphabetic(8)+"@test.co.za";
+	}
+	
+	public static String getJSONString(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();		
+		
+		return objectMapper.writeValueAsString(obj);
 	}
 
 }
